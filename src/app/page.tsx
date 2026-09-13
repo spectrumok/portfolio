030da9b5c6bd7ae509dbf12f7ef0e6efd4b24cad
spectrumok <61932297+spectrumok@users.noreply.github.com>
2026-09-13 08:32:01 +0200

    descriptive past work section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Motion } from "@/components/motion";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -74,21 +75,52 @@ export default function Home() {
         </div>
       </Motion>
 
-      <Motion direction="down" delay={2} className="pt-35">
+      <Motion direction="down" delay={2} className="pt-10 flex flex-col items-center justify-center gap-2">
         <p className="text-3xl">my past work</p>
+        <p className="text-muted-foreground">below you&apos;ll find all previous projects i&apos;ve worked on</p>
 
-        <div className="w-full gap-2 pt-5">
-          <div className="flex">
-            <a className="underline text-xl" href="https://www.roblox.com/communities/34382694/Going-Ape-Game">
-              Going Ape
-            </a>
+        <Separator />
+
+        <div className="w-full flex flex-col gap-1 pt-5">
+          <div className="flex flex-col gap-2">
+            <p className="text-xl">Going Ape</p>
+
+            <div className="flex gap-1">
+              <Link href="https://www.roblox.com/communities/34382694/Going-Ape-Game">
+                <Button size={"xs"} variant="outline">
+                  <Image
+                    alt="ROBLOX"
+                    src="/roblox.svg"
+                    width={16}
+                    height={16}
+                  />
+
+                  ROBLOX Group
+                </Button>
+              </Link>
+
+              <Link href="https://discord.gg/8EC5Bwhp3v">
+                <Button size={"xs"} variant="outline">
+                  <Image
+                    alt="Discord"
+                    src="/discord.svg"
+                    width={16}
+                    height={16}
+                  />
+
+                  Discord Server
+                </Button>
+              </Link>
+            </div>
           </div>
 
-          <p className="text-muted-foreground">developer • June 2025 - current</p>
+          <p className="text-muted-foreground">Developer • June 2025 - current</p>
+
+          <p className="text-wrapped w-full max-w-100">At Going Ape, I work with a professional development team and am responsible for numerous tasks, including but not limited to creating and maintaining systems, efficent problem solving, handling Server Authority mechanics, assisting with React-based UI components, and regular scripting duties.</p>
         </div>
       </Motion>
 
-      <a className="mt-auto underline text-muted-foreground text-xs" href="">
+      <a className="mt-auto underline text-muted-foreground text-xs pt-10" href="https://github.com/spectrumok/portfolio">
         find the code for this portfolio here
       </a>
     </main>
